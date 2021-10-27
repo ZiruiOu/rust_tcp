9d9f19fc4ddb5353e891b789e0c8caf6fee31c88
ZiruiOu <1800012945@pku.edu.cn>
2021-10-27 02:38:46 -0700

    feat(link/src/device.rs): add code for struct Device with test

--- link/src/device.rs
+++ link/src/device.rs
@@ -1,7 +1,9 @@
-use super::common::ipaddrs::IpAddress;
-use super::common::macaddr::MacAddress;
-use pcap::{Capture, Device};
+use common::ipaddr::IpAddress;
+use common::macaddr::MacAddress;
+use core::result::Result;
+use pcap::{self, Active, Capture};
 use pnet::datalink::{self, NetworkInterface};
+use pnet::ipnetwork::IpNetwork;
 
 pub struct Device {
     device_name: String,
@@ -13,25 +15,25 @@ pub struct Device {
 impl Device {
     pub fn new(name: &str) -> Self {
         let mut handler = Capture::from_device(name)
-            .expect(format!("Device: device {} not exists.", name))
+            .expect(format!("Device: device {} not exists.", name).as_str())
             .promisc(true)
             .immediate_mode(true)
             .snaplen(6000)
             .open()
-            .expect(format!(
-                "Device: device {} cannot initialize handler.",
-                name
-            ));
+            .expect(
+                format!("Device: device {:?} cannot initialize handler.", name)
+                    .as_str(),
+            );
 
         let mut handler = handler
             .setnonblock()
             .expect("Device: sent non-block error.");
 
         let mut mac_address = MacAddress::new();
-        Device::init_mac_address(&mut mac_address);
+        Device::init_mac_address(name, &mut mac_address);
 
         let mut ip_address = IpAddress::new();
-        Device::init_ip_address(&mut ip_address);
+        Device::init_ip_address(name, &mut ip_address);
 
         Device {
             device_name: String::from(name),
@@ -50,7 +52,7 @@ impl Device {
 
         match netif.mac {
             Some(address) => {
-                mac_addr = MacAddress::from(address.octets());
+                *mac_addr = MacAddress::from(address.octets());
             }
             None => {
                 panic!("init_mac_address: device mac address doesn't exist.");
@@ -61,15 +63,68 @@ impl Device {
     fn init_ip_address(name: &str, ip_addr: &mut IpAddress) {
         // naive implementation using utilities.
         let netif = datalink::interfaces()
+            .into_iter()
             .filter(|iface: &NetworkInterface| iface.name == name)
             .next()
             .expect("init_ip_address: device nnot found.");
 
         for network in netif.ips.iter() {
-            if let V4(ipv4_network) = network {
-                ip_addr = IpAddress::from(ipv4_network.ip().octets());
+            if let IpNetwork::V4(ipv4_network) = network {
+                *ip_addr = IpAddress::from(ipv4_network.ip().octets());
                 return;
             }
         }
+    }
+
+    pub fn send_frame(
+        &mut self,
+        message: &String,
+        ethernet_type: u16,
+        dest_mac: &MacAddress,
+    ) -> Result<(), pcap::Error> {
+        let buffer = Device::create_frame(
+            &(*self).mac_addr,
+            dest_mac,
+            ethernet_type,
+            message,
+        );
+        (*self).pcap_handler.sendpacket(buffer.as_slice())?;
+        Ok(())
+    }
+
+    fn create_frame(
+        src_mac: &MacAddress,
+        dest_mac: &MacAddress,
+        ethernet_type: u16,
+        message: &String,
+    ) -> Vec<u8> {
+        let mut buffer = Vec::<u8>::new();
+        let mut type_buffer = u16::to_be_bytes(ethernet_type);
+
+        buffer.extend_from_slice(dest_mac.as_slice());
+        buffer.extend_from_slice(src_mac.as_slice());
+        buffer.extend_from_slice(&mut type_buffer);
+        buffer.extend_from_slice((*message).as_bytes());
+
+        buffer
+    }
+
+    pub fn receive_poll(&mut self) {
+        loop {
+            while let Ok(packet) = (*self).pcap_handler.next() {
+                println!("{:?}", packet);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_init_device() {
+        let device_name = "veth1-2";
+        let device = Device::new(device_name);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 use common::ipaddr::IpAddress;
 use common::macaddr::MacAddress;
+use link::device::Device;
 use std::io;
 use std::str::FromStr;
 
 fn main() {
-    println!("{}", "Hello World!");
+    let mut input_buffer = String::new();
+    io::stdin()
+        .read_line(&mut input_buffer)
+        .expect("stdin(): read_line error.");
+
+    let mut inputs = input_buffer.trim().split_whitespace();
+
+    let command = inputs.next().unwrap();
+    let device_name = inputs.next().unwrap();
+
+    match command {
+        "send" => {
+            let mut sender = Device::new(device_name);
+            let dest_mac =
+                MacAddress::from_str(inputs.next().unwrap()).unwrap();
+            let message = String::from("Hello, how are you?");
+            sender.send_frame(&message, 0x2333, &dest_mac).unwrap();
+        }
+        _ => {
+            let mut receiver = Device::new(device_name);
+            receiver.receive_poll();
+        }
+    }
 }
